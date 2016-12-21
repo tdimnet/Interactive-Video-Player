c52b522889c08f151cc103163c0ab320c30423a6
Thomas Dimnet <Thomas.dimnet@gmail.com>
2016-12-21 09:29:00 +0100

    Add the functions which control the video when it's played, pause and when the sound is mute and unmute. Also add the styles to each element which appends to the page

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var videoElement = videoContainer.querySelector('video');
 var videoControlsContainer = videoContainer.querySelector('div');
 var playButton = document.createElement("img");
 var pauseButton = document.createElement("img");
-//Think to add the input range for the duration of the video
+var progressBar = document.createElement("input");
 var volumeOffIcon = document.createElement("img");
 var volumeOnIcon = document.createElement("img");
 var fullscreenIcon = document.createElement("img");
@@ -26,6 +26,7 @@ videoElement.removeAttribute('controls');
 
 // Adding classes and styles to the elements
   //Set the class for each icon
+    //Think to add the progressBar attributes here
 playButton.setAttribute('class', 'button playButton');
 pauseButton.setAttribute('class', 'button pauseButton');
 volumeOffIcon.setAttribute('class', 'button volumeOffIcon');
@@ -42,6 +43,69 @@ fullscreenIcon.setAttribute('src', 'icons/fullscreen-icon.png');
 
 
 // Adding the elements to the page
+    //videoControlsContainer.append(progressBar); => add it once it's ok!
+videoControlsContainer.append(playButton);
+videoControlsContainer.append(volumeOnIcon);
+videoControlsContainer.append(fullscreenIcon);
+
+
+
+// Creating the functions
+var playTheVideo = function() {
+  console.log("And the video is now playing!");
+  //Start the video
+  videoElement.play();
+  //Remove the play button
+  videoControlsContainer.removeChild(playButton);
+  //Add the pause button
+  videoControlsContainer.append(pauseButton);
+}
+
+var stopTheVideo = function() {
+  console.log("And the video is now stop!");
+  //Pause the video
+  videoElement.pause();
+  //Remove the pause button
+  videoControlsContainer.removeChild(pauseButton);
+  //Add the play button
+  videoControlsContainer.append(playButton);
+}
+
+var muteTheVideo = function() {
+  console.log("And the sound of the video is now gone!");
+  //Mute the video
+  videoElement.muted = true;
+  //Remove the volume ON button
+  videoControlsContainer.removeChild(volumeOnIcon);
+  //Add the volume OFF button
+  videoControlsContainer.append(volumeOffIcon);
+}
+
+var unmuteTheVideo = function() {
+  console.log("And the sound of the video is now back!");
+  //Unmute the video
+  videoElement.muted = false;
+  //Remove the volume OFF button
+  videoControlsContainer.removeChild(volumeOffIcon);
+  //Add the volume ON button
+  videoControlsContainer.append(volumeOnIcon);
+}
+
+
+
+//When the play button is clicked
+playButton.onclick = playTheVideo;
+
+//When the pause button is clicked
+pauseButton.onclick = stopTheVideo;
+
+//When the volume ON button is clicked
+volumeOnIcon.onclick = muteTheVideo;
+
+//When the volume OFF button is clicked
+volumeOffIcon.onclick = unmuteTheVideo;
+
+
 
 
 
